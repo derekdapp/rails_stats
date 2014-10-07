4706d37c53d6b212a9047a14dabca47685307d84
derekdapp <derekdapp@gmail.com>
2014-10-07 13:10:29 -0600

    added a has_many

--- lib/rails_stats/stats.rb
+++ lib/rails_stats/stats.rb
@@ -6,14 +6,23 @@ module LowLevel
     CURRENT_FILE = File.basename(__FILE__)
     PIPE_MINUS_CURRENT_FILE = "| grep -v \"#{CURRENT_FILE}\""
     HAS_MANY_SEARCH = "ack \" has_many \" -c | awk -F \":\" '{print $2,$1}' | grep -v \"0\" #{PIPE_MINUS_COVERAGE} #{PIPE_MINUS_CURRENT_FILE} | sort -rn"
-
+    LOC_SEARCH = "find . -iname \"*.rb\" -type f -exec wc -l {} \; | sort -rn"
+    LINES_COUNT_SEARCH = "find . -iname \"*.rb\" -type f -exec cat {} \; | wc -l"
     def self.get_raw(command)
-      # ["3 app/models/user.rb", "3 app/models/project.rb"]
       `#{command}`
     end
 
     def self.get_has_many_relationships
       get_raw(HAS_MANY_SEARCH).split("\n")
     end
+
+    def self.get_files_sorted_loc
+      get_raw(LOC_SEARCH).split("\n")
+    end
+
+    def self.get_lines_of_code
+      get_raw(LINES_COUNT_SEARCH).split("\n")
+    end
+
   end
 end
